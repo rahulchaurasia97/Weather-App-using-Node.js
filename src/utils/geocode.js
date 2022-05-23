0b071167646f1d093f675400968450fd65300441
Rahul Chaurasia <rahul.chaurasia@costrategix.com>
2022-05-23 16:12:31 +0530

    this is weather app project using node.js

---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoicmFodWwtY2hhdXJhc2lhIiwiYSI6ImNsM2lqZ21qMjFnYWozYm5yanI5OXZ4ZG0ifQ.0V8-e4p7YfQnI-qk0Wb9pA&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
+      });
+    }
+  });
+};
+
+module.exports = geocode;
